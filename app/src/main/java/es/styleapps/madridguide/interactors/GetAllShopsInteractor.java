09b8001cc115cb81aee9322daf5c113828080150
jlgarciaap <jlgarciaap@gmail.com>
2017-01-18 21:36:28 +0100

    Cache 7 dias funcionando a falta de refactorizar

--- app/src/main/java/es/styleapps/madridguide/interactors/GetAllShopsInteractor.java
+++ app/src/main/java/es/styleapps/madridguide/interactors/GetAllShopsInteractor.java
@@ -28,49 +28,56 @@ public class GetAllShopsInteractor {
         long actualDate = new Date().getTime();
         long dateInPref = dateSaved.getLong(PREFS_NAME,actualDate);
         ShopDAO dao = new ShopDAO(context);
+        boolean daoData = false;
 
-        if(actualDate - dateInPref > Constants.SEVENDAYS || dao.query().isEmpty()){
+            if (dao.query() != null){
+                if (!dao.query().isEmpty()){
 
+                    daoData = true;
+                }
+            }
 
-            dao.deleteAll();
-            NetworkManager networkManager = new NetworkManager(context);
-            networkManager.getShopsFromServer(new NetworkManager.GetShopListener() {
-                @Override
-                public void getShopEntitiesSuccess(List<ShopEntity> result) {
-                    List<Shop> shops = new ShopEntityShopMapper().map(result);
+            if (actualDate - dateInPref > Constants.SEVENDAYS || !daoData  || dateInPref == actualDate) {
 
-                    if (response != null) {
-                        response.response(Shops.build(shops));
+                dao.deleteAll();
+                NetworkManager networkManager = new NetworkManager(context);
+                networkManager.getShopsFromServer(new NetworkManager.GetShopListener() {
+                    @Override
+                    public void getShopEntitiesSuccess(List<ShopEntity> result) {
+                        List<Shop> shops = new ShopEntityShopMapper().map(result);
 
-                    }
+                        if (response != null) {
+                            response.response(Shops.build(shops));
+
+                        }
 //
-                    SharedPreferences dateSaved = context.getSharedPreferences(PREFS_NAME, 0);
-                    SharedPreferences.Editor editor = dateSaved.edit();
-                    editor.putLong(PREFS_NAME, new Date().getTime());
-                    editor.commit();
+                        SharedPreferences dateSaved = context.getSharedPreferences(PREFS_NAME, 0);
+                        SharedPreferences.Editor editor = dateSaved.edit();
+                        editor.putLong(PREFS_NAME, new Date().getTime());
+                        editor.commit();
 
 //                //TODO actualizar cache cada 7 dias para que no descargue y grabe siempre que descargue
 //                ShopDAO dao = new ShopDAO(getApplicationContext());
 //                for (Shop shop : shops) {
 //                    dao.insert(shop);
 //                }
-                }
-
-                @Override
-                public void getShopEntitiesDidFail() {
-
-                }
-            });
-
-        } else {
-            //Si es mayor de siete dias borramos  y descagamos de nuevo
-
-
-                    List<Shop> shopsList = dao.query();
-                    if (response != null) {
-                        response.response(Shops.build(shopsList));
                     }
 
-        }
+                    @Override
+                    public void getShopEntitiesDidFail() {
+
+                    }
+                });
+
+            } else {
+                //Si es mayor de siete dias borramos  y descagamos de nuevo
+
+                List<Shop> shopsList = dao.query();
+                if (response != null) {
+                    response.response(Shops.build(shopsList));
+                }
+
+            }
+
     }
 }
